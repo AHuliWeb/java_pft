f393219ce1a96420f761dcc029c1aa83fae94db4
Barval <barval@barval.com>
2020-06-16 17:36:55 +0300

    added some functions

--- sandbox/src/main/java/ru_stqa_pft_sandbox/MyFirstProgramm.java
+++ sandbox/src/main/java/ru_stqa_pft_sandbox/MyFirstProgramm.java
@@ -1,7 +1,25 @@
 package ru_stqa_pft_sandbox;
 public class MyFirstProgramm {
     public static void main (String[] args){
-        System.out.println("Hello, Mars!");
+        hello ("World");
+        hello ("Alexa");
+        double l = 5;
+        System.out.println("area of square with side " + l + " = " + area(l));
+        double a = 4;
+        double b = 6;
+        System.out.println("area of triangel with sides " + a + " and " + b + " = " + area(a, b));
+        }
+    public static void hello (String someone){
+        System.out.println("Hello, " + someone + "!");
+
     }
+    public static double area (double len){
+            return len*len;
+    }
+    public static double area (double a, double b){
+        return a*b;
+    }
+
+
 }
 
